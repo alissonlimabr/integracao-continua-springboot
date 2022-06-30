f717bb367073715e9005ccfb8d32d66c91917e2b
Alisson Lima <101370736+alissonlimabr@users.noreply.github.com>
2022-06-30 16:04:52 -0500

    Restaurando filtro CORS: Teste finalizado!

--- src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
+++ src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
@@ -17,7 +17,6 @@ public class FiltroCors {
         CorsConfiguration configCors = new CorsConfiguration();
         configCors.setAllowCredentials(true);
         configCors.setAllowedOrigins(Arrays.asList("https://webacademy-grupo5.herokuapp.com"));
-        configCors.setAllowedOrigins(Arrays.asList("https://front-end-angular-mu.vercel.app"));
         configCors.setAllowedMethods(Arrays.asList("*"));
         configCors.setAllowedHeaders(Arrays.asList("*"));
 
