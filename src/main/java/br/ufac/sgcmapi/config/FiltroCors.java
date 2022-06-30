8b6fb5ce1a64fa0498d0f2e548a30fac97eb4ef9
Alisson Lima <101370736+alissonlimabr@users.noreply.github.com>
2022-06-30 15:37:59 -0500

    Update FiltroCors.java

--- src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
+++ src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
@@ -16,8 +16,8 @@ public class FiltroCors {
 
         CorsConfiguration configCors = new CorsConfiguration();
         configCors.setAllowCredentials(true);
-        configCors.setAllowedOrigins(Arrays.asList("https://webacademy-grupo5.herokuapp.com"));
-        configCors.setAllowedMethods(Arrays.asList("https://front-end-angular-mu.vercel.app"));
+        configCors.setAllowedOrigins(Arrays.asList("https://front-end-angular-mu.vercel.app"));
+        configCors.setAllowedMethods(Arrays.asList("*"));
         configCors.setAllowedHeaders(Arrays.asList("*"));
 
         UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
